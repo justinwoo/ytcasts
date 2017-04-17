2b8d15f2b531fa1c7db3bdc02bdd528a4c33fdb6
justinwoo <moomoowoo@gmail.com>
2017-04-17 11:04:17 +0300

    update for 0.11 and use more purescript

--- src/Main.purs
+++ src/Main.purs
@@ -1,29 +1,152 @@
 module Main where
 
-import YTCasts.Exe
-import Control.Monad.Aff (Canceler)
-import Control.Monad.Aff.Console (CONSOLE)
+import Prelude
+import Network.HTTP.Affjax as Affjax
+import Control.Monad.Aff (Aff, Canceler, launchAff, makeAff)
+import Control.Monad.Aff.Console (errorShow, log)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Exception (EXCEPTION)
-import Network.HTTP.Affjax (AJAX)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Exception (EXCEPTION, Error)
+import Control.Monad.Eff.Ref (REF, newRef)
+import Control.Monad.Except (runExcept)
+import Data.Array (length)
+import Data.Either (Either(..))
+import Data.Foldable (for_)
+import Data.Foreign.Class (class Decode)
+import Data.Foreign.Generic (decodeJSON, defaultOptions, genericDecode)
+import Data.Generic.Rep (class Generic)
+import Data.Maybe (Maybe(..))
+import Data.Traversable (for)
+import Network.HTTP.Affjax (AJAX, URL)
+import Node.ChildProcess (CHILD_PROCESS, StdIOBehaviour(..), defaultSpawnOptions, onError, onExit, spawn, toStandardError)
+import Node.Encoding (Encoding(..))
 import Node.FS (FS)
-import SQLite3 (DBEffects)
+import Node.FS.Aff (readTextFile)
+import SQLite3 (DBConnection, DBEffects, closeDB, newDB, queryDB)
+import Text.Parsing.StringParser (ParseError)
+import Unsafe.Coerce (unsafeCoerce)
+
+type HTMLString = String
+
+type Query = String
+
+type Param = String
+
+runDownload :: forall e.
+  URL ->
+  Aff
+    ( ref :: REF
+    , cp :: CHILD_PROCESS
+    | e
+    )
+    (Either Error String)
+runDownload url = makeAff \e s -> do
+  ref <- newRef ""
+  process <- spawn "youtube-dl"
+             [ "-o"
+             , "downloads/%(title)s.%(ext)s"
+             , "-x"
+             , "--audio-format"
+             , "mp3"
+             , url
+             ]
+             $ defaultSpawnOptions { stdio = [Just Pipe] }
+  onError process $ toStandardError >>> Left >>> s
+  onExit process $ const (s $ Right "success?")
+
+newtype Config = Config
+  { targets :: Array URL }
+derive instance genericConfig :: Generic Config _
+instance decodeConfig :: Decode Config where
+  decode = genericDecode $ defaultOptions { unwrapSingleConstructors = true }
+
+type Cast =
+  { title :: String
+  , link :: URL
+  }
+
+data CastStatus
+  = CastAlreadyDownloaded
+  | CastDownloaded Cast
+  | CastDownloadFailed Error Cast
+
+downloadCast ::
+  forall e.
+  DBConnection ->
+  Cast ->
+  Aff
+    (Program e)
+    CastStatus
+downloadCast conn cast = do
+  exists <- (\rows -> 1 == length (unsafeCoerce rows)) <$> queryDB conn "SELECT 1 from downloads where link = ?" [cast.link]
+  case exists of
+    true -> pure CastAlreadyDownloaded
+    false -> do
+      result <- runDownload cast.link
+      case result of
+        Right _ -> do
+          _ <- queryDB conn "INSERT INTO downloads (link, title, created) VALUES ($1, $2, datetime('now'));" [cast.link, cast.title]
+          log $ "downloaded " <> cast.title <> " from " <> cast.link
+          pure $ CastDownloaded cast
+        Left e -> do
+          log $ "cast download failed of " <> cast.title <> " " <> show e
+          pure $ CastDownloadFailed e cast
+
+foreign import _getCasts :: HTMLString -> Array Cast
+getCasts :: HTMLString -> Either ParseError (Array Cast)
+getCasts = pure <<< _getCasts
+-- getCasts s = do
+--   tags <- parseTags s
+--   pure $ foldMap getLinks tags
+--   where
+--     getLinks (TagOpen (TagName "a") attrs) = do
+--       case {title: _, link: _}
+--         <$> getAttr "title" attrs
+--         <*> getAttr "href" attrs
+--         of
+--         Just a -> pure a
+--         Nothing -> mempty
+--     getLinks _ = mempty
+--     getAttr match xs = getValue <$> find matchName xs
+--       where
+--         matchName (Attribute (Name name) _) = match == name
+--         getValue (Attribute _ (Value x)) = decode <<< trim $ x
+
+downloadCasts ::
+  forall e.
+  DBConnection ->
+  String ->
+  Aff
+    (Program e)
+    (Array CastStatus)
+downloadCasts conn url = do
+  res <- Affjax.get url
+  case getCasts res.response of
+    Right casts -> for casts $ downloadCast conn
+    Left e -> do
+      errorShow e
+      pure []
+
+type Program e =
+  ( ajax :: AJAX
+  , console :: CONSOLE
+  , ref :: REF
+  , cp :: CHILD_PROCESS
+  , fs :: FS
+  , db :: DBEffects
+  | e
+  )
 
 main :: forall e.
   Eff
-    ( ajax :: AJAX
-    , console :: CONSOLE
-    , fs :: FS
-    , db :: DBEffects
-    , err :: EXCEPTION
-    | e
-    )
-    (Canceler
-       ( ajax :: AJAX
-       , console :: CONSOLE
-       , fs :: FS
-       , db :: DBEffects
-       | e
-       )
-    )
-main = mainExe
+    (Program (exception :: EXCEPTION | e))
+    (Canceler (Program e))
+main = launchAff do
+  decoded <- decodeJSON <$> readTextFile UTF8 "./config.json"
+  case runExcept decoded of
+    Right (Config config) -> do
+      conn <- newDB "./data"
+      for_ config.targets $ (downloadCasts conn)
+      closeDB conn
+    Left e -> do
+      errorShow e
