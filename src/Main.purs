92e0338c4bb4df15d714c83dcbe6944f15213687
justinwoo <moomoowoo@gmail.com>
2018-04-18 20:04:27 +0300

    make this less stupid by merging the results of casts

--- src/Main.purs
+++ src/Main.purs
@@ -2,24 +2,25 @@ module Main where
 
 import Prelude
 
-import Control.Monad.Aff (Aff, launchAff_)
+import Control.Monad.Aff (Aff, bracket, launchAff_)
 import Control.Monad.Aff.Console (errorShow, log)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Ref (REF)
 import Control.Monad.Rec.Class (Step(..), tailRec)
-import Data.Array (find, head, length, snoc)
+import Data.Array as Array
 import Data.Either (Either(..))
-import Data.Foldable (for_)
-import Data.List ((:))
-import Data.Maybe (Maybe(..))
+import Data.Foldable (and, find)
+import Data.List (List, (:))
+import Data.List as List
+import Data.Maybe (Maybe(Just), fromMaybe)
 import Data.Monoid (mempty)
 import Data.Newtype (class Newtype, unwrap)
 import Data.String (Pattern(..), contains, split, trim)
 import Data.String.HtmlElements (decode)
-import Data.Traversable (for)
-import Data.Tuple (Tuple(..))
+import Data.Traversable (traverse, traverse_)
+import Data.Tuple (Tuple(Tuple))
 import LenientHtmlParser (Attribute(..), Name(..), Tag(..), TagName(..), Value(..), parseTags)
 import Milkis as M
 import Node.ChildProcess as CP
@@ -89,7 +90,7 @@ downloadCast ::
     (Program e)
     CastStatus
 downloadCast conn cast = do
-  exists <- (\rows -> 1 == length (unsafeCoerce rows)) <$> queryDB conn "SELECT 1 from downloads where link = ?" [unwrap cast.link]
+  exists <- (\rows -> 1 == Array.length (unsafeCoerce rows)) <$> queryDB conn "SELECT 1 from downloads where link = ?" [unwrap cast.link]
   case exists of
     true -> pure CastAlreadyDownloaded
     false -> do
@@ -105,7 +106,7 @@ downloadCast conn cast = do
           log $ "cast download failed of " <> cast.title <> " " <> show e
           pure $ CastDownloadFailed e cast
 
-getCasts :: HTMLString -> Either ParseError (Array Cast)
+getCasts :: HTMLString -> Either ParseError (List Cast)
 getCasts s = do
   tags <- parseTags s
   pure $ tailRec getLinks (Tuple mempty tags)
@@ -113,8 +114,8 @@ getCasts s = do
     getLinks (Tuple acc (TagOpen (TagName "a") attrs : TNode tnode : TagClose (TagName "a") : xs))
       | Just true <- contains (Pattern "yt-uix-tile-link") <$> (getAttr "class" attrs)
       , title <- trim tnode
-      , Just (Just href) <- head <<< split (Pattern "&") <$> getAttr "href" attrs
-      , link <- Url $ "https://www.youtube.com" <> href = Loop (Tuple (snoc acc {title, link}) xs)
+      , Just (Just href) <- Array.head <<< split (Pattern "&") <$> getAttr "href" attrs
+      , link <- Url $ "https://www.youtube.com" <> href = Loop (Tuple (List.Cons {title, link} acc) xs)
       | otherwise = Loop (Tuple acc xs)
     getLinks (Tuple acc (_ : xs)) = Loop (Tuple acc xs)
     getLinks (Tuple acc _) = Done acc
@@ -123,20 +124,29 @@ getCasts s = do
         matchName (Attribute (Name name) _) = match == name
         getValue (Attribute _ (Value x)) = decode <<< trim $ x
 
-downloadCasts ::
+fetchCasts ::
   forall e.
   DBConnection ->
   Url ->
   Aff
     (Program e)
-    (Array CastStatus)
-downloadCasts conn (Url url) = do
+    (List Cast)
+fetchCasts conn (Url url) = do
   res <- M.text =<< M.fetch (M.URL url) M.defaultFetchOptions
   case getCasts res of
-    Right casts -> for casts $ downloadCast conn
+    Right casts ->
+      pure casts
     Left e -> do
       errorShow e
-      pure []
+      pure mempty
+
+ensureDB :: forall e. DBConnection -> Aff (Program e) Unit
+ensureDB conn =
+  void $ queryDB conn """
+CREATE TABLE IF NOT EXISTS downloads
+(link varchar(20) primary key unique, title varchar, created datetime);
+""" []
+
 
 type Program e =
   ( console :: CONSOLE
@@ -156,9 +166,31 @@ main = launchAff_ do
   decoded <- readJSON <$> readTextFile UTF8 "./config.json"
   case decoded of
     Right (config :: Config) -> do
-      conn <- newDB "./data"
-      _ <- queryDB conn "CREATE TABLE IF NOT EXISTS downloads (link varchar(20) primary key unique, title varchar, created datetime);" []
-      for_ config.targets $ (downloadCasts conn)
-      closeDB conn
+      bracket (newDB "./data") closeDB (withConn $ List.fromFoldable config.targets)
     Left e -> do
       errorShow e
+  where
+    withConn targets conn = do
+      ensureDB conn
+      log "Fetching targets..."
+      casts <- merge <$> traverse (fetchCasts conn) targets
+      log $ "Found " <> show (List.length casts) <> " targets."
+      traverse_ (downloadCast conn) casts
+
+    -- merge these damn lists
+    merge :: List (List Cast) -> List Cast
+    merge xs = iter mempty xs
+
+    -- what a mess
+    iter :: List Cast -> List (List Cast) -> List Cast
+    iter xs ys =
+      case and (List.null <$> ys) of
+        true -> xs
+        _ ->
+          let
+            heads :: List Cast
+            heads = List.catMaybes $ List.head <$> ys
+            tails :: List (List Cast)
+            tails = (fromMaybe mempty <<< List.tail) <$> ys
+          in
+          iter (heads <> xs) tails
